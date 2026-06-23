04455ae4e74a2e8758f66566791ae827faeb103b
prdaxca <100952967+FORTY71@users.noreply.github.com>
2026-06-23 21:46:28 +0800

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,48 @@
+export default function handler(req, res) {
+  // Ambil key dari URL (GET) atau dari body aplikasi (POST)
+  const key = (req.body && req.body.key) || req.query.key;
+
+  // Cek Key
+  if (key !== 'GHOST-VIP-KEY') {
+    return res.status(401).json({ 
+      status: 'failed', 
+      message: 'Key salah. Jika buka di browser, gunakan format URL: /api/login?key=GHOST-VIP-KEY' 
+    });
+  }
+
+  // Output JSON murni statis
+  const responseJson = {
+    "access": "∞",
+    "credit": "30",
+    "device": "null",
+    "devices": {
+      "037e73973a3b9e17": {
+        "last_login": "23-06-2026 16:48",
+        "name": "CPH2467",
+        "registered_at": "23-06-2026 16:48"
+      },
+      "05e16c88de678041": {
+        "last_login": "23-06-2026 17:22",
+        "name": "V2039",
+        "registered_at": "23-06-2026 17:22"
+      },
+      "14ccd0f38e99d0d1": {
+        "last_login": "23-06-2026 17:18",
+        "name": "CPH2423",
+        "registered_at": "23-06-2026 17:18"
+      },
+      "15c786e42807ed0a": {
+        "last_login": "23-06-2026 17:17",
+        "name": "25028PC03G",
+        "registered_at": "23-06-2026 16:37"
+      }
+    },
+    "rgtime": "23-06-2026 13:49",
+    "status": "active",
+    "username": "GHOST",
+    "validity": "24-06-2026 13:49"
+  };
+
+  // Kirim response
+  return res.status(200).json(responseJson);
+}
